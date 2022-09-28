caa726df57ed7d3830b3b1b42128a456b457e322
jemeryhsiao <1402223078xxj@gmail.com>
2022-09-28 10:04:12 +0800

    log:[initialization project]

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+       <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
